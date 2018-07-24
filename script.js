f7a3e58670a5ef46a5d5aa69e3b3efc92e4ca1ff
roma0152 <roma0152@bitbucket.org>
2018-07-24 19:06:40 +0300

    recurcive try to solve

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-var pos, fen, chess;
+var chess, board, randMove, gameMoves, turn, moveCounter = 0;
 
-var board = ChessBoard('board', {
+board = ChessBoard('board', {
   draggable: true,
   dropOffBoard: 'trash',
   sparePieces: true
 });
 
 
-function findSolution() {
-   pos = board.position();
-   fen = board.fen();
+function findSolution() {  
+   chess = new Chess();
+   if (moveCounter % 2 == 0) turn = 'w'; else turn = 'b';
+   var tmp  =board.position('fen')+' '+turn+' KQkq - 0 1';
+   //console.log(chess.validate_fen(tmp));
+   chess.load(tmp);
+   gameMoves = chess.ugly_moves();
+   randMove = gameMoves[Math.floor(Math.random() * gameMoves.length)]
+
+     statusCall();
+
+    chess.ugly_move(randMove);
+    board.position(chess.fen());
+
+    moveCounter++;
+
+
    
-   tmp_fen = '8/8/6Q1/4K3/8/8/3k4/8';
-   chess = new Chess(tmp_fen);
-   board.fen(tmp_fen);
-   
-   console.log(chess.ugly_moves());
-   console.log(chess.game_over());
-   console.log(chess.fen());
+}
+
+
+
+function clearTask() {
+  board.clear();
+  chess.clear();
+
+  statusCall();
+}
+
+function statusCall() {
+
+  console.log("Board Current position: "+board.fen());
+   console.log("Algorithm Current position: "+chess.fen());
+
+   console.log("in_check ["+chess.in_check()+"]");
+   console.log("in_checkmate ["+chess.in_checkmate()+"]");
+   console.log("in_draw ["+chess.in_draw()+"]");
+   console.log("=======================\nAll MOVES: \n");
+   console.log(gameMoves);
+   console.log("----------------------------\nSelect Move: \n");
+   console.log(randMove);
+   //console.log("Is game over ["+chess.game_over()+"]");
+
+    console.log("----------------------------\nDone !: \n");
 
 }
 
 $('#solve').on('click', findSolution);
+$('#clear').on('click', clearTask);
